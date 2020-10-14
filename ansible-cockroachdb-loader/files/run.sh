432b6c27d9ecbfe8fcf8c7eb6bab48250b8b81c8
Ivan Prisyazhnyy <john.koepi@gmail.com>
2020-10-14 15:51:09 +0200

    ycsb pg can't in mt

--- ansible-cockroachdb-loader/files/run.sh
+++ ansible-cockroachdb-loader/files/run.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-N=3
-RECORD=1000000000
+N=100
+RECORD=100000000
 
 read -r -a NODES <<< $(cat /nodes_internal)
 NODES_LEN=${#NODES[@]}
@@ -13,11 +13,11 @@ NODES_LEN=${#NODES[@]}
 echo "round robin through nodes: ${NODES[@]}"
 
 for i in $(seq 0 $(($N-1)))
-do 
+do
   NODE=${NODES[$(($i%$NODES_LEN))]}
   echo "$NODE"
   screen -L -S "ycsb-run-$i" -d -m /opt/ycsb/bin/ycsb run postgrenosql \
-    -threads 64 \
+    -threads 1 \
     -P /opt/ycsb/workloads/workloada \
     -p measurementtype=hdrhistogram \
     -p insertorder=ordered -p zeropadding=12 \
@@ -25,6 +25,7 @@ do
     -p postgrenosql.url=jdbc:postgresql://$NODE:26257/ycsb \
     -p postgrenosql.user=root -p postgrenosql.passwd= \
     -p postgrenosql.autocommit=true \
-    -p fieldcount=10 -p fieldlength=128
+    -p fieldcount=10 -p fieldlength=128 \
+    -p operationcount=100000000
 done
 
